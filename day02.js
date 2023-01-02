94d242232e4bc307794cb9bc396d6daef52e136a
Almassy <119798414+Almassy@users.noreply.github.com>
2023-01-02 12:56:51 -0300

    dia 01 & 02

---
+++ day02.js
@@ -0,0 +1,9 @@
+/*- Qual o seu nome?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando?
+
+
+No final, o sistema vai exibir a mensagem:
+
+"Olá [nome], você tem [idade] anos e já está aprendendo [linguagem]!" */
+
