28efbf0a8ccb0b343a63b4e0974ecb7578d8fee3
Almassy <119798414+Almassy@users.noreply.github.com>
2023-01-07 10:28:13 -0300

    Exercicio complementar day2

--- day02.js
+++ day02.js
@@ -12,9 +12,32 @@ No final, o sistema vai exibir a mensagem:
 var language = "linguagem"
 
 var name = "nome" */
-
+    function show (phrase){
+        document.write(phrase)
+    }
     var name = prompt ("Olá, qual seu nome?");
     var age = prompt (name + ", qual sua idade?");
     var language = prompt (name + ", qual linguagem você está estudando?");
 
-document.write ("Olá " + name + " você tem " + age + " anos e já está aprendendo " + language);
+show ("Olá " + name + " você tem " + age + " anos e já está aprendendo " + language);
+
+/*Você vai complementar o código para que, depois de exibir a mensagem anterior, o programa pergunte:
+
+Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.
+
+E aí, dependendo da resposta, ele deve mostrar uma das seguintes mensagens:
+
+1 > Muito bom! Continue estudando e você terá muito sucesso.
+2 > Ahh que pena... Já tentou aprender outras linguagens?*/
+
+function like (){
+    let ask = prompt ("Você está gostando de estudar " + language + "? <p> 1 = SIM</p><p>2 = Não</p> ");
+
+    if (ask = 1){
+        show("<br> Muito bom! Continue estudando e você terá muito sucesso!");
+    }
+    if (ask = 2) { show ("<br> Ahh que pena... Já tentou aprender outras linguagens?");}
+    if (ask !=1 !=2) {show ("<br> Preciso que aperte F5 e responda corretamente a última pergunta")}
+}
+
+like();
